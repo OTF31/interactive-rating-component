bb9c9c11324c5c7faa19fd78cf0834679554e2c1
denis <182906@unsaac.edu.pe>
2022-03-26 11:39:19 -0500

    Add body app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
+// bg-[#252D37] dark blue
+
+const App = () => {
   return (
-    <div className="text-red-900 text-4xl font-bold mx-auto container">
-      Hola
+    <div className="w-full h-screen bg-[#121417] flex justify-center items-center">
+      <div className="bg-[#252D37] w-[325px] h-[360px] rounded-2xl">
+        {/*Star*/}
+
+        {/*Title*/}
+
+        {/*Paragraph*/}
+
+        {/*Buttons*/}
+
+        {/*Sumbit*/}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
